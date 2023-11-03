69102ec819eb689eb935d6df59208f5a11692ed8
umemasiddiqi <149755257+umemasiddiqi@users.noreply.github.com>
2023-11-03 10:37:35 +0500

    Update pak_points.R

--- 2023/11012023/pak_points.R
+++ 2023/11012023/pak_points.R
@@ -1,7 +1,8 @@
 ##################################
-### 30 Day Map Challenge #########
-### Nov 1, 2023. Points    #######
-#### Umema A. Siddiqi ############
+### 30 Day Map Challenge  ########
+###      Nov 1, 2023.     ########
+###      Day 1: Points    ########
+####    Umema A. Siddiqi #########
 ##################################
 
 # libraries ---------------------------------------------------------------
@@ -24,10 +25,10 @@ pak<- map_data('world')[map_data('world')$region == "Pakistan",]
 
 # get the bounding box of points ------------------------------------------
 # Calculate the range of longitude and latitude
-min_lon <- min(pak$long - 6)
-max_lon <- max(pak$long + 6)
-min_lat <- min(pak$lat - 7)
-max_lat <- max(pak$lat + 7)
+min_lon <- min(pak$long - 5)
+max_lon <- max(pak$long + 5)
+min_lat <- min(pak$lat - 6)
+max_lat <- max(pak$lat + 6)
 
 # calculate the grid of points --------------------------------------------
 df_points <- expand_grid(x = seq(from = min_lon, to = max_lon, length.out = 100),
@@ -46,15 +47,15 @@ map_df <- data.frame(PtInPoly(df_points, pak_map_line)) %>%
 
 # create plot -------------------------------------------------------------
 ggplot() +
-  geom_sf(data = map_df, size = 0.05, color = "#000000") +
+  geom_sf(data = map_df, size = 0.09, color = "darkgreen") +
   coord_sf() +
   theme_void() +
-  theme(plot.caption = element_text(family = font, hjust = 0.9, size = 7.5, color = "#000000", margin = margin(t = 25)),
+  theme(plot.caption = element_text(family = font, hjust = 0.9, size = 3.5, color = "darkblue", margin = margin(t = 25)),
         plot.caption.position = "plot",
         plot.margin = unit(c(0.5, 0.5, 0, 0.5), "cm"),
         panel.background = element_rect(color = NA, fill = "#f7f7f5"),
         plot.background = element_rect(color = NA, fill = "#f7f7f5")) +
-  labs(caption = "#30DayMapChallenge | Day 1: Points | Design: Umema Siddiqi")
+  labs(caption = "#30DayMapChallenge | Day 1: Points | Design: U. Siddiqi")
 
 # save plot ---------------------------------------------------------------
 ggsave(paste0("us_neg_points_", format(Sys.time(), "%d%m%Y"), ".png"), dpi = 320, width = 6, height = 6)
